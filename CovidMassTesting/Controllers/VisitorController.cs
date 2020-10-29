b1acff56a41251c09b42106d393fa57e981d5064
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2020-10-29 09:01:03 +0100

    email and phone validation for registration

--- CovidMassTesting/Controllers/VisitorController.cs
+++ CovidMassTesting/Controllers/VisitorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CovidMassTesting.Model;
 using CovidMassTesting.Repository;
@@ -33,12 +34,15 @@ namespace CovidMassTesting.Controllers
         }
         /// <summary>
         /// Public method for pre registration. Result is returned with Visitor.id which is the main identifier of the visit and should be shown in the bar code
+        /// 
+        /// Request size is limitted.
         /// </summary>
         /// <param name="visitor"></param>
         /// <returns></returns>
         [HttpPost("Register")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
+        [RequestSizeLimit(2000)]
         public async Task<ActionResult<Visitor>> Register([FromBody] Visitor visitor)
         {
             try
@@ -47,6 +51,23 @@ namespace CovidMassTesting.Controllers
                 {
                     throw new ArgumentNullException(nameof(visitor));
                 }
+
+                try
+                {
+                    var addr = new System.Net.Mail.MailAddress(visitor.Email);
+                    visitor.Email = addr.Address;
+                }
+                catch
+                {
+                    visitor.Email = "";
+                }
+
+                visitor.Phone = FormatPhone(visitor.Phone);
+                if (!IsPhoneNumber(visitor.Phone))
+                {
+                    visitor.Phone = visitor.Phone;
+                }
+
                 var place = await placeRepository.GetPlace(visitor.ChosenPlaceId);
                 if (place == null) { throw new Exception("We are not able to find chosen testing place"); }
                 var slotM = await slotRepository.Get5MinSlot(visitor.ChosenPlaceId, visitor.ChosenSlot);
@@ -68,6 +89,30 @@ namespace CovidMassTesting.Controllers
                 return BadRequest(new ProblemDetails() { Detail = exc.Message });
             }
         }
-
+        /// <summary>
+        /// Format phone to slovak standard
+        /// 
+        /// 0800 123 456 convers to +421800123456
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string FormatPhone(string number)
+        {
+            if (number == null) number = "";
+            number = number.Replace(" ", "");
+            number = number.Replace("\t", "");
+            if (number.StartsWith("00")) number = "+" + number.Substring(2);
+            if (number.StartsWith("0")) number = "+421" + number.Substring(1);
+            return number;
+        }
+        /// <summary>
+        /// Validates the phone number +421800123456
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsPhoneNumber(string number)
+        {
+            return Regex.Match(number, @"^(\+[0-9]{12})$").Success;
+        }
     }
 }
