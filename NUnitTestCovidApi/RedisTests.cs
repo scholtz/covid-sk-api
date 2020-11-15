78b48f3fbe7f4e25ec8d55c51e5b9ba631fc1483
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2020-11-15 12:27:29 +0100

    do not do redis tests in release

--- NUnitTestCovidApi/RedisTests.cs
+++ NUnitTestCovidApi/RedisTests.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿//#define DoRedisTests
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NUnitTestCovidApi
 {
-#if DEBUG
+#if DoRedisTests
     public class RedisTests : Tests
     {
         public override string AppSettings { get; set; } = "redis-appsettings.json";
