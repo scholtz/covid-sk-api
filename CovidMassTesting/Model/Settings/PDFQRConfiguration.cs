44167b8578ff70c4c286ad469aead7d0106fdd78
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2021-01-19 20:25:02 +0100

    tune for one page

--- CovidMassTesting/Model/Settings/PDFQRConfiguration.cs
+++ CovidMassTesting/Model/Settings/PDFQRConfiguration.cs
@@ -12,21 +12,21 @@ namespace CovidMassTesting.Model.Settings
     {
         public string Prefix { get; set; } = "BA01";
         public long Increment { get; set; } = 3;
-        public long OffsetBranch { get; set; } = 100000;
+        public long OffsetBranch { get; set; } = 1000000;
         public int OffsetIter { get; set; } = 2;
         public int Columns { get; set; } = 3;
-        public int Count { get; set; } = 100;
+        public int Count { get; set; } = 8 * 3 * 3;
         public float Height { get; set; } = 50f;
-        public float Width { get; set; } = 200f;
-        public float CellPaddingTop { get; set; } = 5F;
+        public float Width { get; set; } = 180f;
+        public float CellPaddingTop { get; set; } = 0;
         public float CellPaddingRight { get; set; } = 10F;
-        public float CellPaddingLeft { get; set; } = 10F;
-        public float CellPaddingBottom { get; set; } = 5F;
-        public float PageMarginTop { get; set; } = 10f;
+        public float CellPaddingLeft { get; set; } = 20F;
+        public float CellPaddingBottom { get; set; } = 0;
+        public float PageMarginTop { get; set; } = 15f;
         public float PageMarginRight { get; set; } = 10f;
         public float PageMarginLeft { get; set; } = 10f;
         public float PageMarginBottom { get; set; } = 10f;
         public string Type { get; set; } = "QRCode";
-        public float Scale { get; set; } = 2.5f;
+        public float Scale { get; set; } = 2.1f;
     }
 }
