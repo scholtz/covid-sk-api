151fab290a0c3b357ac093bf4b58d13c7bd3ed5f
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2021-03-18 18:23:26 +0100

    update CompanyRegistrationsExport

--- CovidMassTesting/Controllers/UserController.cs
+++ CovidMassTesting/Controllers/UserController.cs
@@ -405,7 +405,7 @@ namespace CovidMassTesting.Controllers
         /// </summary>
         /// <returns></returns>
         [Authorize]
-        [HttpGet("CompanyRegistrationsExport")]
+        [HttpPost("CompanyRegistrationsExport")]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         public async Task<ActionResult> CompanyRegistrationsExport([FromQuery] int from = 0, [FromQuery] int count = 9999999)
